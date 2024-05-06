715f63e343974dcd3fa2d5f2dc6a1bc8d49da20c
MikeB1124 <balianmichael@gmail.com>
2024-05-05 17:16:38 -0700

    adding local and remote test functions to print

---
+++ main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/MikeB1124/escpos"
+)
+
+func TestLocalPrinting(t *testing.T) {
+	ip := "192.168.86.29"
+	port := "9100"
+	p := escpos.PrinterConfig{}
+	p.InitPrinter(ip, port)
+
+	err := p.WriteToPrinter(getEscCommands(ip, port))
+	if err != nil {
+		t.Errorf("error trying to pring: %+v", err)
+		return
+	}
+	p.NetConnection.Close()
+	t.Log("Printed Receipt!")
+}
+
+func TestRemotePrinting(t *testing.T) {
+	ip := "23.243.244.219"
+	port := "7100"
+	p := escpos.PrinterConfig{}
+	p.InitPrinter(ip, port)
+
+	err := p.WriteToPrinter(getEscCommands(ip, port))
+	if err != nil {
+		t.Errorf("error trying to pring: %+v", err)
+		return
+	}
+	p.NetConnection.Close()
+	t.Log("Printed Receipt!")
+}
+
+func getEscCommands(ip string, port string) []byte {
+	var escCmdBuffer bytes.Buffer
+	escCmdBuffer.Write(escpos.Init)
+	escCmdBuffer.Write(escpos.CenterAlign)
+	escCmdBuffer.Write(escpos.DoubleHeightMode)
+	escCmdBuffer.Write(escpos.LineFeed)
+	escCmdBuffer.Write(escpos.StringToHexBytes(fmt.Sprintf("IPv4 Address: %s", ip)))
+	escCmdBuffer.Write(escpos.LineFeed)
+	escCmdBuffer.Write(escpos.StringToHexBytes(fmt.Sprintf("Port: %s", port)))
+	escCmdBuffer.Write(escpos.LineFeed)
+	escCmdBuffer.Write(escpos.LineFeed)
+	escCmdBuffer.Write(escpos.LineFeed)
+	escCmdBuffer.Write(escpos.FeedPaperAndCut)
+	return escCmdBuffer.Bytes()
+}
